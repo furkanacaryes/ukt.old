b36b258e51671357a04fc31861be811c4d7cceee
Furkan Acar <furkan@kimbukedi.com>
2019-04-21 18:59:41 +0300

    Routes changed as being relative to routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'hakkimizda', loadChildren: 'app/about/about.module#AboutModule' },
-  { path: 'servisler',  loadChildren: 'app/services/services.module#ServicesModule' },
-  { path: 'urunler',    loadChildren: 'app/products/products.module#ProductsModule' },
-  { path: 'iletisim',   loadChildren: 'app/contact/contact.module#ContactModule' },
-  { path: '',           loadChildren: 'app/home/home.module#HomeModule' },
+  { path: 'hakkimizda', loadChildren: './about/about.module#AboutModule' },
+  { path: 'servisler',  loadChildren: './services/services.module#ServicesModule' },
+  { path: 'urunler',    loadChildren: './products/products.module#ProductsModule' },
+  { path: 'iletisim',   loadChildren: './contact/contact.module#ContactModule' },
+  { path: '',           loadChildren: './home/home.module#HomeModule' },
 ];
 
 @NgModule({
