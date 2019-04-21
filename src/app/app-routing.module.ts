762b29ac0d21ca45df5256f67d5659ffad6e9137
Furkan Acar <furkan@kimbukedi.com>
2019-04-21 18:44:06 +0300

    App Routing Module Configured.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',           loadChildren: 'app/home/home.module#HomeModule' },
+  { path: 'hakkimizda', loadChildren: 'app/about/about.module#AboutModule' },
+  { path: 'servisler',  loadChildren: 'app/services/services.module#ServicesModule' },
+  { path: 'urunler',    loadChildren: 'app/products/products.module#ProductsModule' },
+  { path: 'iletisim',   loadChildren: 'app/contact/contact.module#ContactModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
