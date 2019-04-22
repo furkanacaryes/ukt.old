869a56ed117903993b88c8ed7a22f1d843f52d01
Furkan Acar <furkan@kimbukedi.com>
2019-04-22 19:41:34 +0300

    Menu Placed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ukt';
+
+  menuItems: Array<{name: string, routerLink: string, sub?: Array<{}>}> = [
+    { name: 'Anasayfa',   routerLink: '/' },
+    { name: 'Hakkımızda', routerLink: '/hakkimizda' },
+    { name: 'Servisler',  routerLink: '/servisler', sub: [
+      { name: 'Aa', routerLink: '/servisler/a' },
+      { name: 'Bb', routerLink: '/servisler/b' },
+      { name: 'Cc', routerLink: '/servisler/c' }
+    ]},
+    { name: 'Ürünler',    routerLink: '/urunler' },
+    { name: 'İletişim',   routerLink: '/iletisim' },
+  ];
 }
