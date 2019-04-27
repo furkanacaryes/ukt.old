a7ea8f35886ed67487453175dbe84b1c2f5ecaa5
emreacar <acaremre@me.com>
2019-04-27 20:53:29 +0300

    Completed.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,33 +7,40 @@ import { trigger, transition, query, group, style, animate } from '@angular/anim
   styleUrls: ['./products.component.scss'],
   animations: [
     trigger('showOff', [
-      transition(':enter', [
+      transition(':enter', group([
         query('.content', [
           style({
             opacity: 0,
             filter: 'blur(8px)',
-            transform: 'perspective(800px) translateZ(200px)'
+            transform: 'perspective(800px) translateZ(300px)'
           }),
           animate('600ms ease', style({
             opacity: 1,
             filter: 'none',
             transform: 'perspective(800px) translateZ(0)'
           }))
+        ]),
+        query('.overlay', [
+          style({ opacity: 0 }),
+          animate('600ms ease', style({ opacity: 1 }))
         ])
-      ]),
+      ])),
 
       transition(':leave', group([
         query('.content', [
           animate('600ms ease', style({
             opacity: 0,
             filter: 'blur(8px)',
-            transform: 'perspective(800px) translateZ(200px)'
+            transform: 'perspective(800px) translateZ(300px)'
           }))
         ]),
         query('#clone', [
           animate('600ms ease', style({
             transform: 'translate3d(0, 0, 0) scale(1)'
           }))
+        ]),
+        query('.overlay', [
+          animate('600ms ease', style({ opacity: 0 }))
         ])
       ]))
     ])
