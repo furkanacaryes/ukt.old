740cb3b9f17211dfc70a400ff828a232d6f474ae
Furkan Acar <furkan@kimbukedi.com>
2019-05-27 15:08:57 +0300

    Optimizations.

--- src/app/Animations/showOff.animation.ts
+++ src/app/Animations/showOff.animation.ts
@@ -13,12 +13,12 @@ export default trigger('showOff', [
       style({
         opacity: 0,
         filter: 'blur(8px)',
-        transform: 'perspective(800px) translateZ(300px)'
+        transform: 'translateZ(300px)'
       }),
       animate('600ms ease', style({
         opacity: 1,
         filter: 'none',
-        transform: 'perspective(800px) translateZ(0)'
+        transform: 'translateZ(0)'
       }))
     ]),
     query('.overlay', [
@@ -32,7 +32,7 @@ export default trigger('showOff', [
       animate('600ms ease', style({
         opacity: 0,
         filter: 'blur(8px)',
-        transform: 'perspective(800px) translateZ(300px)'
+        transform: 'translateZ(300px)'
       }))
     ]),
     query('#clone', [
